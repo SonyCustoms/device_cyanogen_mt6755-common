54a5be5a96eb501c463a2a4f92beb1cb300ab11a
Andrew <poisk567@gmail.com>
2018-11-23 09:15:49 +0200

    fix

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -31,7 +31,6 @@ $(call add-rootdir-targets, \
     init.aee.rc \
     init.qns.rc \
     init.rc \
-    init.mal.rc \
     meta_init.connectivity.rc \
     meta_init.modem.rc \
     meta_init.project.rc \
